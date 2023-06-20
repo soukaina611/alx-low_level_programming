0720b06e760dcbcb1ac934686303762ac0f56302
soukaina611 <sk.sekkat@gmail.com>
2023-06-19 18:23:46 -0700

    add 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -18,7 +18,7 @@ printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(x));
 printf("Size of a int: %lu byte(s)\n", (unsigned long)sizeof(y));
 printf("Size of long int: %lu byte(s)\n", (unsigned long)sizeof(z));
 printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(e));
-printf("Size of a float: %lu byte(s)\n", sizeof(unsigned long)sizeof(f));
+printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 
 return (0);
 }
