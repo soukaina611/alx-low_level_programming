f0b8a646cd42125922ff2b4588cdc5f88d16952c
soukaina611 <sk.sekkat@gmail.com>
2023-06-21 14:42:36 -0700

    add 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,23 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * main - entry point
- * Description:'write progrqm that prints _putchar'
+ * Description:'write program that prints _putchar'
  * Return: Always 0 means success
 */
 
 int main(void)
 {
-	char word = "_putchar";
 
-	printf(word);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
 	return (0);
 }
