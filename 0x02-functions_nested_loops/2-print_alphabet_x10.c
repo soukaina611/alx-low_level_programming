011493aab91b9fe50d7aae56d64d3c5fa6710c36
soukaina611 <sk.sekkat@gmail.com>
2023-06-21 15:38:11 -0700

    add 2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet - function print all the alphabet
+ * Description:'prints 10 times the alphabet, in lowercase'
+ * Return:void
+ */
+
+void print_alphabet_x10(void)
+{
+        char alphabet;
+	int i = 0;
+
+	while (i <= 9)
+	{
+        for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+        {
+                _putchar(alphabet);
+        }
+        _putchar('\n');
+	i++;
+	}
+}
