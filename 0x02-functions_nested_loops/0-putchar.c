57459d9a8b11a880ca76d8d9b3473c6809d80ef5
soukaina611 <sk.sekkat@gmail.com>
2023-06-21 09:35:59 -0700

    add 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description:'write progrqm that prints _putchar'
+ * Return: Always 0 means success
+*/
+
+int main(void)
+{
+	char word = "_putchar";
+
+	printf(word);
+	return (0);
+}
