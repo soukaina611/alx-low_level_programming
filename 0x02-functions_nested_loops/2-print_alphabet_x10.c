91597b9cf8f73c8444eb82a1e1ad2b0fde187290
soukaina611 <sk.sekkat@gmail.com>
2023-06-21 16:51:59 -0700

    add 3-islower.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - function print 10 times all the alphabet
+ * print_alphabet_x10 - function print 10 times2-print_alphabet_x10.c all the alphabet
  * Description:'prints 10 times the alphabet, in lowercase'
  * Return:void
 */
---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _islower - function checks lowercase character
+ * @c:character to check
+ * Return:1 if is lowercase else 0
+*/
+
+int _islower(int c)
+{
+
+if  (c >= 97 && c <= 122)
+	return (1);
+return (0);
+
+}
