37024d39543e0285a1722182380af5b8b1241abb
soukaina611 <sk.sekkat@gmail.com>
2023-06-21 18:18:23 -0700

    add 9-times_table.c

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+ * _putchar function : write the caracter c  to stdout
+ * &c is a character that we want to print
+ * Return 1 on Success, -1 on Error
+*/
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+
+}
