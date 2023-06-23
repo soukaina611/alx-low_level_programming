5d96cf240266bcf963eaba2fec77382366f7b0c3
soukaina611 <sk.sekkat@gmail.com>
2023-06-23 15:06:03 -0700

    add 0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *_isupper - function checks for uppercase character
+ * @c: the number to check
+ * Return: 1 for uppercase letter else 0
+*/
+
+int _isupper(int c)
+{
+for (c >= 65 ; c <= 90 ; c++)
+{
+return (1);
+}
+return (0);
+}
