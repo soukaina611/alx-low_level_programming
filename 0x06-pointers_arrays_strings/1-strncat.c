53718f0f8ab31cdf1f8593b13094118db5f83fe1
soukaina611 <sk.sekkat@gmail.com>
2023-06-30 16:05:03 -0700

    add 1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *_strncat - function concatenate 2 strings
+ *@dest:string value
+ *@src:string value
+ *@n:string value
+ *Return: string
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+int a = 0;
+int b = 0;
+
+while (dest[a] != '\0')
+a++;
+while (src[b] != '\0' && b < n)
+{
+dest[a]=src[b];
+a++;
+b++;
+}
+dest[a] = '\0';
+return (dest);
+}
