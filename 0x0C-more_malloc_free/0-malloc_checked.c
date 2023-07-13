cb5aa062d7e6e1432ab4a111b13d2390c6f34c52
soukaina611 <sk.sekkat@gmail.com>
2023-07-13 16:22:17 -0700

    add 0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *malloc_checked - function allocate memory using malloc
+ *@b:integer
+ *Return: a poiter to the first area of memory or 98
+*/
+
+void *malloc_checked(unsigned int b)
+{
+int *ar = malloc(b);
+
+if (ar == 0)
+{
+return (98);
+}
+return (ar);
