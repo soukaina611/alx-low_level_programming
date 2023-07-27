34260f89c04d16f0000a03dda22d0c44cfe55851
soukaina611 <sk.sekkat@gmail.com>
2023-07-27 15:17:27 -0700

    add 101-hello_holberton.asm

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,11 @@
+section             .text
+        extern      printf
+	global      main
+
+main:
+         mov               edi, message
+         mov               eax, 0
+         call       printf
+
+section             .data
+         message db 'Hello, Holberton',10
