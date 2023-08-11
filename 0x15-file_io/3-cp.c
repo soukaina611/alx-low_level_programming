d03527b23a0fc1430a38f4a0b3952a442a8d5263
soukaina611 <sk.sekkat@gmail.com>
2023-08-11 14:49:04 -0700

    add 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,70 +1,72 @@
 #include "main.h"
 #define usage_error "Usage: cp file_from file_to\n"
+#define noread_error "Error: Can't read from file %s\n"
 #define nowrite_error "Error: Can't write to %s\n"
 #define file_permissions (S_IWUSR | S_IRUSR | S_IWGRP | S_IRGRP | S_IROTH)
 #define closefile_error "Error: Can't close fd %d\n"
-#define noread_error "Error: Can't read from file %s\n"
+
 /**
 *main - funct exec prog
-*@argumentc:the counter of args
-*@argumentv:the vecto args
+*@argsc:the counter of args
+*@argsv:the vecto args
 *Return: if there is an error 0 else 1 means success
 */
-int main(int argumentc, char *argumentv[])
+int main(int argsc, char *argsv[])
 {
-int file_source = 0;
-int file_dest = 0;
+int file_source;
+int file_dest;
 char *storage;
-ssize_t read_file;
+int read_file, write_file;
 
-file_source = open(argumentv[1], O_RDONLY);
-if (file_source == -1)
+file_source = open(argsv[1], O_RDONLY);
+storage = malloc(sizeof(char) * 1024);
+if (storage == NULL)
 {
-dprintf(STDERR_FILENO, noread_error, argumentv[1]);
+dprintf(STDERR_FILENO, nowrite_error, argsv[2]);
+exit(99);
+}
+read_file = read(file_source, storage, 1024);
+
+file_dest = open(argsv[2], O_CREAT | O_WRONLY | O_TRUNC, file_permissions);
+
+do {
+
+if (read_file == -1 || file_source == -1)
+{
+dprintf(STDERR_FILENO, noread_error, argsv[1]);
 exit(98);
 }
 
-if (argumentc != 3)
+if (argsc != 3)
 {
 dprintf(STDERR_FILENO, usage_error);
 exit(97);
 }
-file_dest = open(argumentv[2], O_CREAT | O_WRONLY | O_TRUNC, file_permissions);
-if (file_dest == -1)
+
+write_file = write(file_dest, storage, read_file);
+
+if (file_dest == -1 || write_file == -1)
 {
-dprintf(STDERR_FILENO, nowrite_error, argumentv[2]);
-exit(98);
-}
-storage = malloc(sizeof(char) * 1024);
-read_file = read(file_source, storage, 1024);
-if (read_file == -1)
-{
-dprintf(STDERR_FILENO, noread_error, argumentv[1]);
+dprintf(STDERR_FILENO, nowrite_error, argsv[2]);
 free(storage);
-exit(98);
-}
-while (read_file > 0)
-{
-if (write(file_dest, storage, read_file) != read_file)
-{
-dprintf(STDERR_FILENO, nowrite_error, argumentv[2]);
-free(storage);
+
 exit(99);
 }
-}
+read_file = read(file_source, storage, 1024);
+file_dest = open(argsv[2], O_WRONLY | O_APPEND);
+} while (read_file > 0);
+free(storage);
 file_source = close(file_source);
 file_dest = close(file_dest);
-if (file_dest)
-{
-dprintf(STDERR_FILENO, closefile_error, file_source);
-exit(100);
-}
 if (file_source)
 {
 dprintf(STDERR_FILENO, closefile_error, file_source);
 exit(100);
 }
-
-return (1);
+if (file_dest)
+{
+dprintf(STDERR_FILENO, closefile_error, file_source);
+exit(100);
 }
-
+return (0);
+}
